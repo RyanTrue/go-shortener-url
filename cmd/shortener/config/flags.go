a5e78d8234dca43f9a047e25e3251b76b9e63dec
RyanTrue <zufirka@icloud.com>
2023-05-09 22:42:48 +0300

    minor fix

--- cmd/shortener/config/flags.go
+++ cmd/shortener/config/flags.go
@@ -7,6 +7,6 @@ var DefaultAddr string
 
 func ParseFlags() {
 	flag.StringVar(&ServerAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&DefaultAddr, "b", "https://localhost:8080", "address and port to run server")
+	flag.StringVar(&DefaultAddr, "b", "http://localhost:8080", "address and port to run server")
 	flag.Parse()
 }
