7007c4327a4ee94a2cab1a810668898c43765843
RyanTrue <zufirka@icloud.com>
2023-05-19 22:50:54 +0300

    refactoring

---
+++ cmd/shortener/handler/shortenURL.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+)
+
+func (h *Handler) ShortenURL(c *gin.Context) {
+	body := c.Request.Body
+
+	defer func(body io.ReadCloser) {
+		if err := body.Close(); err != nil {
+			fmt.Printf("Failed to close body: %v", err)
+		}
+	}(body)
+
+	data, err := io.ReadAll(body)
+	if err != nil {
+		http.Error(c.Writer, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	if len(data) == 0 {
+		http.Error(c.Writer, "", http.StatusBadRequest)
+		return
+	}
+
+	bodyStr := string(data)
+	shortURL := h.services.URL.ShortenURL(bodyStr)
+
+	c.String(http.StatusCreated, shortURL)
+}
---
+++ cmd/shortener/service/service.go
@@ -0,0 +1,16 @@
+package service
+
+import "github.com/RyanTrue/go-shortener-url.git/cmd/shortener/config"
+
+type ServiceContainer struct {
+	URL urlService
+}
+
+func NewServiceContainer(repo map[string]string, config config.AppConfig) *ServiceContainer {
+	return &ServiceContainer{
+		URL: urlService{
+			repo:   repo,
+			config: config,
+		},
+	}
+}
