ac40f569931c5b2506dc9e12e6d8f4e6eeb7e0b7
RyanTrue <zufirka@icloud.com>
2023-05-20 00:19:16 +0300

    fix

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -13,10 +12,6 @@ func (s *Server) Run(port string, handler http.Handler) error {
 	s.httpServer = &http.Server{
 		Addr:    port,
 		Handler: handler,
-	}
-	err := s.httpServer.ListenAndServe()
-	if err != nil {
-		fmt.Printf("Server failed: ", err.Error())
 	}
 	return s.httpServer.ListenAndServe()
 }
